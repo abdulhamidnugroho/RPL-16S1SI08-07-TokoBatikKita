336aea3899174a85efc6dede38baf707d138184b
galangcp <cgalang38@yahoo.com>
2018-11-23 21:06:43 +0700

    test

---
+++ Project/Barang/ClsEntBarang.vb
@@ -0,0 +1,48 @@
+﻿Public Class ClsEntBarang
+    Private idbarang As String
+    Private namabarang As String
+    Private hargabeli As Integer
+    Private hargajual As Integer
+    Private jumlahstok As Integer
+
+    Public Property idbarang() As String
+        Get
+            Return idbarang
+        End Get
+        Set(value As String)
+            idbarang = value
+        End Set
+    End Property
+    Public Property namabarang() As String
+        Get
+            Return namabarang
+        End Get
+        Set(value As String)
+            namabarang = value
+        End Set
+    End Property
+    Public Property hargabeli() As Integer
+        Get
+            Return hargabeli
+        End Get
+        Set(value As Integer)
+            hargabeli = value
+        End Set
+    End Property
+    Public Property hargajual() As Integer
+        Get
+            Return hargajual
+        End Get
+        Set(value As Integer)
+            hargajual = value
+        End Set
+    End Property
+    Public Property jumlahstok() As Integer
+        Get
+            Return jumlahstok
+        End Get
+        Set(value As Integer)
+            jumlahstok = value
+        End Set
+    End Property
+End Class
---
+++ Project/Barang/FormBarang.vb
@@ -0,0 +1,9 @@
+﻿Public Class FormBarang
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub FormBarang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
