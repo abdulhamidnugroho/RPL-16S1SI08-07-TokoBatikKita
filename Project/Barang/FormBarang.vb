1c008cc2cdcbdfcf75c2f4da2e74715b8a046556
galangcp <43667188+galangcp@users.noreply.github.com>
2018-11-30 19:42:26 +0700

    Delete FormBarang.vb

--- Project/Barang/FormBarang.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class FormBarang
-    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
-
-    End Sub
-
-    Private Sub FormBarang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
